f53e5a6b906eee12ec65c082473ea47b3fc63c4f
Borja Ros <brosp@iesch.org>
2025-10-27 18:13:53 +0100

    Proceso10 Mensaje Statistics

--- src/ed/ud4/Average.java
+++ src/ed/ud4/Average.java
@@ -1,36 +1,49 @@
 package ed.ud4;
-import java.time.LocalTime;
+
 import java.util.Random;
 
 public class Average {
 
     public static void main(String[] args) {
-        // Crear un objeto Random para generar números aleatorios
         Random random = new Random();
+        System.out.println("Statistics");
 
-        // Obtener un número aleatorio entre 0 y 19 (ambos incluidos)
-        int count = random.nextInt(20);  // nextInt(20) genera un número entre 0 y 19
+        int count = random.nextInt(20);  // entre 0 y 19
         System.out.println("Cantidad de números a generar: " + count);
 
-        // Inicializar una variable para la suma total
         int total = 0;
+        double producto = 1.0;
+        int minimo = Integer.MAX_VALUE;
+        int maximo = Integer.MIN_VALUE;
 
-        // Generar tantos números aleatorios entre 1 y 50 como se haya obtenido en el punto anterior
         for (int i = 0; i < count; i++) {
-            int numeroAleatorio = random.nextInt(100) + 1;  // nextInt(100) genera entre 0 y 99, sumamos 1 para que sea entre 1 y 100
+            int numeroAleatorio = random.nextInt(100) + 1;  // entre 1 y 100
             total += numeroAleatorio;
+            producto *= numeroAleatorio;
+
+            if(numeroAleatorio < minimo){
+                minimo = numeroAleatorio;
+            }
+
+            if(numeroAleatorio > maximo){
+                maximo = numeroAleatorio;
+            }
             System.out.println("Número aleatorio " + (i + 1) + ": " + numeroAleatorio);
         }
 
-        // Mostrar la suma total
         System.out.println("Suma total de los números generados: " + total);
 
-        // Mostrar la media
-        double media = (double) total / (double) count;
-        System.out.println("Media: " + media);
+        if (count > 0) {
+            double media = (double) total / count;
+            double mediaGeometrica = Math.pow(producto, 1.0 / count);
+            System.out.println("Media aritmética: " + media);
+            System.out.println("Media geométrica: " + mediaGeometrica);
+            System.out.println("Valor minimo: " + minimo);
+            System.out.println("Valor maximo: " + maximo);
+        } else {
+            System.out.println("No se han generado números, no se puede calcular la media.");
+        }
 
-        // Mensaje de fin de proceso
-        System.out.println("Fin de proceso");
-
+        System.out.println("End Of Program");
     }
 }
